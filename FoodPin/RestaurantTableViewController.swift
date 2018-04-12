ebe9b23e5c96b6b34010649553960f115c7c2ae4
John <kiyoto_zy@hotmail.com>
2018-04-12 09:47:31 +0800

    update 0947

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -49,7 +49,8 @@ class RestaurantTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cellIdentifier = "Cell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as!
+        RestaurantTableViewCell
 
         // Configure the cell...
         cell.nameLabel.text = restaurantNames[indexPath.row]
@@ -66,7 +67,8 @@ class RestaurantTableViewController: UITableViewController {
 
         let optionMenu = UIAlertController(title: nil, message: "What?", preferredStyle: .actionSheet)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        let checkInActopn = UIAlertAction(title: "Check in", style: .default, handler: { (action: UIAlertAction!) -> Void in
+        let checkInActopn = UIAlertAction(title: "Check in", style: .default, handler: { (action: UIAlertAction!) ->
+                Void in
             let cell = tableView.cellForRow(at: indexPath)
             cell?.accessoryType = .checkmark
             self.restaurantIsVisited[indexPath.row] = true
@@ -75,13 +77,15 @@ class RestaurantTableViewController: UITableViewController {
 
         let callerActionHandler = { (action: UIAlertAction!) -> Void in
 
-            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry", preferredStyle: .alert)
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry",
+                    preferredStyle: .alert)
             alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alertMessage, animated: true, completion: nil)
         }
 
 
-        let callAction = UIAlertAction(title: "Call" + "123-000-\(indexPath.row)", style: .default, handler: callerActionHandler)
+        let callAction = UIAlertAction(title: "Call" + "123-000-\(indexPath.row)", style: .default,
+                handler: callerActionHandler)
 
         optionMenu.addAction(checkInActopn)
         optionMenu.addAction(cancelAction)
@@ -91,9 +95,54 @@ class RestaurantTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: false)
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        super.tableView(tableView, commit: editingStyle, forRowAt: indexPath)
+    fileprivate func extractedFunc(_ detectAction: UIContextualAction, _ shareAction: UIContextualAction) -> UISwipeActionsConfiguration {
+        return UISwipeActionsConfiguration(actions: [detectAction, shareAction])
     }
 
-    table
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) ->
+            UISwipeActionsConfiguration? {
+
+
+        let detectAction = UIContextualAction(style: .destructive, title: "Delete") {
+            (action, sourceview, completionHandler) in
+
+            self.restaurantNames.remove(at: indexPath.row)
+            self.restaurantLocations.remove(at: indexPath.row)
+            self.restaurantTypes.remove(at: indexPath.row)
+            self.restaurantIsVisited.remove(at: indexPath.row)
+            self.restaurantImages.remove(at: indexPath.row)
+
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+
+            completionHandler(true)
+        }
+
+        let shareAction = UIContextualAction(style: .normal, title: "Share") {
+            (action, sourceView, completionHandler) in
+
+            let defaultText = "Just checking in at " + self.restaurantNames[indexPath.row]
+            let activityController: UIActivityViewController
+
+            if let imageToShare = UIImage(named: self.restaurantImages[indexPath.row]) {
+
+                activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+            } else {
+                activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+            }
+
+            self.present(activityController, animated: true, completion: nil)
+            completionHandler(true)
+
+        }
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [detectAction, shareAction])
+
+        return swipeConfiguration
+
+    }
+
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+        let
+
+    }
 }
